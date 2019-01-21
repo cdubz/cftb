f1c771c47ce49f58db93e83e6a236ef5cb544551
Christopher C. Wells <chris@chrxs.net>
2019-01-20 20:25:14 -0800

    Add basic Race Day selection form.

--- src/Controller/ToteBoardController.php
+++ src/Controller/ToteBoardController.php
@@ -2,8 +2,12 @@
 
 namespace App\Controller;
 
+use App\Repository\RaceDayRepository;
 use App\Service\APIConsumer;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ToteBoardController extends AbstractController
@@ -11,11 +15,32 @@ class ToteBoardController extends AbstractController
     /**
      * @Route("/board", name="board")
      */
-    public function board(APIConsumer $apiConsumer)
+    public function board(Request $request, RaceDayRepository $raceDayRepository, APIConsumer $apiConsumer)
     {
-        $apiConsumer->update();
+        $RaceDays = $raceDayRepository->findAll();
+
+        $race_choices = [];
+        foreach ($RaceDays as $RaceDay) {
+            $race_choices[$RaceDay->getDate()->format('Y-m-d')] = $RaceDay->getId();
+        }
+        $form = $this->createFormBuilder()
+            ->add('id', ChoiceType::class, ['choices' => $race_choices])
+            ->add('save', SubmitType::class, ['label' => 'Load'])
+            ->getForm();
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $data = $form->getData();
+            $RaceDay = $raceDayRepository->find($data['id']);
+        }
+        else {
+            $RaceDay = $raceDayRepository->findMostRecent();
+        }
+
         return $this->render('tote_board/index.html.twig', [
-            'controller_name' => 'ToteBoardController',
+            'form' => $form->createView(),
+            'race_day' => $RaceDay,
         ]);
     }
 }
--- src/Repository/RaceDayRepository.php
+++ src/Repository/RaceDayRepository.php
@@ -9,7 +9,6 @@ use Symfony\Bridge\Doctrine\RegistryInterface;
 /**
  * @method RaceDay|null find($id, $lockMode = null, $lockVersion = null)
  * @method RaceDay|null findOneBy(array $criteria, array $orderBy = null)
- * @method RaceDay[]    findAll()
  * @method RaceDay[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
  */
 class RaceDayRepository extends ServiceEntityRepository
@@ -17,6 +16,11 @@ class RaceDayRepository extends ServiceEntityRepository
     public function __construct(RegistryInterface $registry)
     {
         parent::__construct($registry, RaceDay::class);
+    }
+
+    public function findAll()
+    {
+        return $this->findBy([], ['date' => 'DESC']);
     }
 
     // /**
@@ -36,15 +40,30 @@ class RaceDayRepository extends ServiceEntityRepository
     }
     */
 
-    /*
-    public function findOneBySomeField($value): ?RaceDay
+    /**
+     * @return RaceDay|null Returns RaceDay object for the specified date.
+     */
+    public function findByDate(\DateTime $dateTime): ?RaceDay
     {
-        return $this->createQueryBuilder('r')
-            ->andWhere('r.exampleField = :val')
-            ->setParameter('val', $value)
+        return $this->createQueryBuilder('rd')
+            ->andWhere('rd.date = :date')
+            ->setParameter('date', $dateTime)
+            ->setMaxResults(1)
+            ->getQuery()
+            ->getOneOrNullResult()
+            ;
+    }
+
+    /**
+     * @return RaceDay|null Returns the most recent RaceDay object.
+     */
+    public function findMostRecent(): ?RaceDay
+    {
+        return $this->createQueryBuilder('rd')
+            ->orderBy('rd.date', 'DESC')
+            ->setMaxResults(1)
             ->getQuery()
             ->getOneOrNullResult()
         ;
     }
-    */
 }
